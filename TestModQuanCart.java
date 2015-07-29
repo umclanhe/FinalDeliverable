b6aefaaf9ab12d26fe526049923c259112401e41
LanHe <umclanhe@users.noreply.github.com>
2015-07-29 12:24:05 -0400

    Update TestModQuanCart.java

--- TestModQuanCart.java
+++ TestModQuanCart.java
@@ -15,8 +15,8 @@ public class TestModQuanCart {
 	    driver.get("https://www.amazon.com");
 	    driver.findElement(By.linkText("Sign in")).click();
 	    driver.findElement(By.id("ap_email")).sendKeys("lah135@pitt.edu");
-		driver.findElement(By.id("ap_password")).sendKeys("umc6080");
-		driver.findElement(By.id("signInSubmit-input")).click();
+	    driver.findElement(By.id("ap_password")).sendKeys("umc6080");
+	    driver.findElement(By.id("signInSubmit-input")).click();
 	}
 	
 // Given I'm logged in and there is one item in my shopping cart,
@@ -27,18 +27,18 @@ public class TestModQuanCart {
 	
 	@Test
 	public void testModifyQuantity1() {
-		driver.findElement(By.id("nav-cart")).click();
-		driver.findElement(By.id("a-autoid-2-announce")).click();
-		driver.findElement(By.id("dropdown1_2")).click();
-		driver.findElement(By.id("nav-cart")).click();
+	    driver.findElement(By.id("nav-cart")).click();
+	    driver.findElement(By.id("a-autoid-2-announce")).click();
+	    driver.findElement(By.id("dropdown1_2")).click();
+	    driver.findElement(By.id("nav-cart")).click();
 		
-		try {
-		WebElement el = driver.findElement(By.id("nav-cart-count"));
+	    try {
+	    WebElement el = driver.findElement(By.id("nav-cart-count"));
 	    String msg = el.getText();
 	    assertTrue(msg.contains("3"));
-		} catch(NoSuchElementException nseex) {
-		fail();			
-		}				
+	    } catch(NoSuchElementException nseex) {
+	    fail();			
+	    }				
 	}
 	
 // Given I'm logged in and there are three items in my shopping cart,
@@ -50,20 +50,20 @@ public class TestModQuanCart {
 
 	@Test
 	public void testModifyQuantity2() {
-		driver.findElement(By.id("nav-cart")).click();
-		driver.findElement(By.id("a-autoid-2-announce")).click();
-		driver.findElement(By.id("dropdown1_9")).click();
-		driver.findElement(By.name("quantityBox")).sendKeys("20");
-		driver.findElement(By.id("a-autoid-3-announce")).click();
-		driver.findElement(By.id("nav-cart")).click();
+	    driver.findElement(By.id("nav-cart")).click();
+	    driver.findElement(By.id("a-autoid-2-announce")).click();
+	    driver.findElement(By.id("dropdown1_9")).click();
+	    driver.findElement(By.name("quantityBox")).sendKeys("20");
+	    driver.findElement(By.id("a-autoid-3-announce")).click();
+	    driver.findElement(By.id("nav-cart")).click();
 		
-		try {
-		WebElement el = driver.findElement(By.id("nav-cart-count"));
+	    try {
+	    WebElement el = driver.findElement(By.id("nav-cart-count"));
 	    String msg = el.getText();
 	    assertTrue(msg.contains("20"));
-		} catch(NoSuchElementException nseex) {
-		fail();			
-		}				
+	    } catch(NoSuchElementException nseex) {
+	    fail();			
+	   }				
 	}
 	
 }
